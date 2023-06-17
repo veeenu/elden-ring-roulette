ab9621b5766951a8f9ca35df90eacb5022af94e0
Andrea Venuta <venutawebdesign@gmail.com>
2023-06-17 12:54:58 +0200

    Add main example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::collections::HashMap;
+
+use elden_ring_roulette::codegen::armaments::ArmamentsSchema;
+
 fn main() {
-    println!("Hello, world!");
+    let armaments: HashMap<String, ArmamentsSchema> =
+        serde_json::from_str(include_str!("../data/objects/armaments.json")).unwrap();
+
+    for (k, v) in armaments {
+        println!("{k}: {v:#?}");
+    }
 }
